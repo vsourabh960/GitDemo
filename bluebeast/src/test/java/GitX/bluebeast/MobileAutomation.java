97aa6eafc676a916f630fa089a397d6133f74ce6
saurabh <vsourabh960@gmail.com>
2025-06-06 20:13:39 +0530

    Asian guy done changes

---
+++ bluebeast/src/test/java/GitX/bluebeast/MobileAutomation.java
@@ -0,0 +1,11 @@
+package GitX.bluebeast;
+
+import org.testng.annotations.Test;
+
+public class MobileAutomation {
+	@Test
+	public void mobileTesting()
+	{
+		System.out.println("This section is for mobile testing");
+	}
+}

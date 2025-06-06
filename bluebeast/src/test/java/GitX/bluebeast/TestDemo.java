0f9ff95e5fb9e3eb63e760cb8e853f067c35468a
saurabh <vsourabh960@gmail.com>
2025-06-06 19:34:30 +0530

    first commit

---
+++ bluebeast/src/test/java/GitX/bluebeast/TestDemo.java
@@ -0,0 +1,12 @@
+package GitX.bluebeast;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	@Test
+	public void firstTest()
+	{
+		System.out.println("Hello, Saurabh this side");
+	}
+	
+}

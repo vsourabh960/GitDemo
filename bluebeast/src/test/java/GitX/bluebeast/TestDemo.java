bdfa62c41bdbffd4e1878a765eb36db50a7c6038
saurabh <vsourabh960@gmail.com>
2025-06-06 19:52:00 +0530

    this is commit from Asian Timezone guy

--- bluebeast/src/test/java/GitX/bluebeast/TestDemo.java
+++ bluebeast/src/test/java/GitX/bluebeast/TestDemo.java
@@ -2,11 +2,14 @@ package GitX.bluebeast;
 
 import org.testng.annotations.Test;
 
+
 public class TestDemo {
 	@Test
 	public void firstTest()
 	{
 		System.out.println("Hello, Saurabh this side");
+		System.out.println("Post jira2");
+		System.out.println("Post jira3");
 	}
 	
 }
